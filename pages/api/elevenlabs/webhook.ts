3f432037bacc63f3a7f02c83b4cf77bba8bface9
Ouail <ouail.impactiq@gmail.com>
2025-08-01 00:22:00 +0200

    webhook.ts console log

--- pages/api/elevenlabs/webhook.ts
+++ pages/api/elevenlabs/webhook.ts
@@ -13,6 +13,7 @@ export const config = {
 };
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    console.log("ALL HEADERS:", JSON.stringify(req.headers, null, 2));
   // ---------- CORS ----------
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST,OPTIONS");
